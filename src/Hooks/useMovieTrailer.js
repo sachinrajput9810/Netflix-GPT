5d489ce65aa92ae527b5a81b85e22e46eadf800f
Sachin Rajput <165654923+sachinrajput9810@users.noreply.github.com>
2024-09-27 15:13:50 +0530

    Did Memoization in Custom hooks to save API calls(Optimize)

--- src/Hooks/useMovieTrailer.js
+++ src/Hooks/useMovieTrailer.js
@@ -2,11 +2,15 @@ import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { addTrailerVideo } from "../Components/movieSlice"
 import { API_OPTIONS } from "../utils/constant"
-
+import { useSelector } from "react-redux"
 
 const useMovieTrailer = (movieID) => {
     
     const dispatch = useDispatch()
+
+    const trailerVideo = useSelector(store => store.movies.trailerVideo)
+
+
     const getMoviesVideo = async() => {
         const data = await fetch('https://api.themoviedb.org/3/movie/957452/videos?language=en-US', API_OPTIONS)
         const json = await data.json()
@@ -18,7 +22,7 @@ const useMovieTrailer = (movieID) => {
     }
 
     useEffect(() => {
-        getMoviesVideo()
+        !trailerVideo && getMoviesVideo()
     } , [])
     
 }
